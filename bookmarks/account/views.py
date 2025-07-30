dce9122658bdf57a3455a431f0a4314084b287ca
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-07-30 13:42:09 +0400

    feat: Create actions' dashboard and some actions
    * In account\views.py
      * Display actions in dashboard
      * Create actions:
        'has created an account',
        'is following'
    * In images\views.py create actions:
      'bookmarked image',
      'likes'

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -7,11 +7,20 @@ from django.views.decorators.http import require_POST
 
 from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
 from .models import Contact, Profile
+from actions.utils import create_action
+from actions.models import Action
 
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions[:10]
+    return render(request, "account/dashboard.html", {"section": "dashboard",
+                                                      'actions': actions})
 
 
 def register(request):
@@ -22,6 +31,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(
                 request, "registration/register_done.html", {"new_user": new_user}
             )
@@ -80,6 +90,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
