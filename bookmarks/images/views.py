01914e7c9d5339e98adb16986435e0067c19a876
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-07-31 14:32:38 +0400

    feat: Add total_views in image_detail rendering

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -5,10 +5,16 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
+import redis
+from django.conf import settings
 
 from .forms import ImageCreateForm
 from .models import Image
 
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
@@ -30,8 +36,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Increase image's total_view by 1 every time
+    total_views = r.incr(f'image:{image.id}:views')
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request, "images/image/detail.html", {"section": "images", "image": image,
+                                              'total_views': total_views}
     )
 
 
